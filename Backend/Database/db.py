a1e866a537a833739b951765c26261f46cc6d288
Ben Loggie <benloggie21@gmail.com>
2025-09-27 16:23:48 +0100

    modified:   Backend/Database/db.py

--- Backend/Database/db.py
+++ Backend/Database/db.py
@@ -6,13 +6,13 @@ import os
 dotenv_path = os.path.join(os.path.dirname(__file__), ".env")
 load_dotenv(dotenv_path)
 
-DB_USER = os.getenv("DB_USER")
-DB_PASSWORD = os.getenv("DB_PASSWORD")
-DB_HOST = os.getenv("DB_HOST")
-DB_PORT = os.getenv("DB_PORT", "5432")
-DB_NAME = os.getenv("DB_NAME")
-DB_PGSSLMODE = os.getenv('DB_PGSSLMODE', "require")
-DB_PGCHANNELBINDING = os.getenv('DB_PGCHANNELBINDING', "require")
+DB_USER = os.getenv("DB_USER").strip()
+DB_PASSWORD = os.getenv("DB_PASSWORD").strip()
+DB_HOST = os.getenv("DB_HOST").strip()
+DB_PORT = os.getenv("DB_PORT", "5432").strip()
+DB_NAME = os.getenv("DB_NAME").strip()
+DB_PGSSLMODE = os.getenv('DB_PGSSLMODE', "require").strip()
+DB_PGCHANNELBINDING = os.getenv('DB_PGCHANNELBINDING', "require").strip()
 
 DATABASE_URL = (
     f"postgresql+psycopg2://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
