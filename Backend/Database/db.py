fa2c355beea7ded8848e7c3fd5e8587aec49de64
Ben Loggie <benloggie21@gmail.com>
2025-09-27 16:20:07 +0100

    new file:   Backend/Database/.env 	modified:   Backend/Database/db.py

--- Backend/Database/db.py
+++ Backend/Database/db.py
@@ -9,10 +9,15 @@ load_dotenv(dotenv_path)
 DB_USER = os.getenv("DB_USER")
 DB_PASSWORD = os.getenv("DB_PASSWORD")
 DB_HOST = os.getenv("DB_HOST")
-DB_PORT = os.getenv("DB_PORT")
+DB_PORT = os.getenv("DB_PORT", "5432")
 DB_NAME = os.getenv("DB_NAME")
+DB_PGSSLMODE = os.getenv('DB_PGSSLMODE', "require")
+DB_PGCHANNELBINDING = os.getenv('DB_PGCHANNELBINDING', "require")
 
-DATABASE_URL = f"postgresql+psycopg2://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
+DATABASE_URL = (
+    f"postgresql+psycopg2://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
+    f"?sslmode={DB_PGSSLMODE}&channel_binding={DB_PGCHANNELBINDING}"
+)
 print("DATABASE_URL =", DATABASE_URL)
 engine = create_engine(DATABASE_URL, echo=True)
 
