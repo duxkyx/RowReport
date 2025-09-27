402758ccf3539d76e6503e9438f19e8a7b3adc87
Ben Loggie <benloggie21@gmail.com>
2025-09-27 14:57:50 +0100

    modified:   Frontend/API_Routes.py

--- Frontend/API_Routes.py
+++ Frontend/API_Routes.py
@@ -1,15 +1,22 @@
-create_user = 'http://127.0.0.1:8000/register'
-check_user = 'http://127.0.0.1:8000/login'
-get_all_users = 'http://127.0.0.1:8000/get_all_users'
+import os
 
-upload_session = 'http://127.0.0.1:8000/upload/session'
-upload_user_data = 'http://127.0.0.1:8000/upload/user_data'
-delete_session = 'http://127.0.0.1:8000/delete/session'
-delete_user = 'http://127.0.0.1:8000/delete/user'
+API_BASE_URL = os.getenv("API_BASE_URL", "http://127.0.0.1:8000")
 
-get_averages = 'http://127.0.0.1:8000/user_data/get_averages'
-get_summary = 'http://127.0.0.1:8000/user_data/get_summary'
-get_radar_averages = 'http://127.0.0.1:8000/user_data/get_radar_averages'
-get_telemetry_data_training_zones = 'http://127.0.0.1:8000/user_data/get_telemetry_data'
-get_sessions = 'http://127.0.0.1:8000/user_data/get_sessions'
-get_rower_data = 'http://127.0.0.1:8000/user_data/get_rower_data'
+def api_route(path: str) -> str:
+    return f"{API_BASE_URL}{path}"
+
+create_user = api_route("/register")
+check_user = api_route("/login")
+get_all_users = api_route("/get_all_users")
+
+upload_session = api_route('/upload/session')
+upload_user_data = api_route('/upload/user_data')
+delete_session = api_route('/delete/session')
+delete_user = api_route('/delete/user')
+
+get_averages = api_route('/user_data/get_averages')
+get_summary = api_route('/user_data/get_summary')
+get_radar_averages = api_route('/user_data/get_radar_averages')
+get_telemetry_data_training_zones = api_route('/user_data/get_telemetry_data')
+get_sessions = api_route('/user_data/get_sessions')
+get_rower_data = api_route('/user_data/get_rower_data')
